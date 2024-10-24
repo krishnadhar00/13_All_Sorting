dc7661a364aea89ab775718953c23ef345f76dd1
krishnadhar pandey <123811773+krishnadhar00@users.noreply.github.com>
2024-10-24 18:48:59 +0530

    Update 05_Quick_Sort.cpp

--- 05_Quick_Sort.cpp
+++ 05_Quick_Sort.cpp
@@ -1,6 +1,7 @@
 // This algorithm also work on the divide and conquer algorithm.
 // QuickSort is a sorting algorithm based on the Divide and Conquer that picks an element as a pivot and partitions the given array around the picked pivot by placing the pivot in its correct position in the sorted array.
 // this is for commiting the github repository
+// this is again another commit
 #include<iostream>
 #include<vector>
 using namespace std;
